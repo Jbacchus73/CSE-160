9c5c23f28de81dd84ec7cb4f0045fe175d96b0a2
justinisaiah73 <justinisaiah73@gmail.com>
2026-04-12 23:21:34 -0700

    modified assignmenet 1

--- asgn1/asg1.js
+++ asgn1/asg1.js
@@ -1,31 +1,33 @@
 // ColoredPoint.js (c) 2012 matsuda
 // Vertex shader program
-var VSHADER_SOURCE =
-  'attribute vec4 a_Position;\n' +
-  'void main() {\n' +
-  '  gl_Position = a_Position;\n' +
-  '  gl_PointSize = 10.0;\n' +
-  '}\n';
+var VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  uniform float u_Size;
+  void main() {
+    gl_Position = a_Position;
+    gl_PointSize = u_Size;
+  }`;
 
 // Fragment shader program
-var FSHADER_SOURCE =
-  'precision mediump float;\n' +
-  'uniform vec4 u_FragColor;\n' +  
-  'void main() {\n' +
-  '  gl_FragColor = u_FragColor;\n' +
-  '}\n';
+var FSHADER_SOURCE = `
+  precision mediump float;
+  uniform vec4 u_FragColor;
+  void main() { 
+    gl_FragColor = u_FragColor;
+  }`;
 
 let canvas; 
 let gl; 
 let a_Position;
 let u_FragColor;
+let u_Size; 
 
 function setupWebGL(){
   // Retrieve <canvas> element
   canvas = document.getElementById('asg1');
 
   // Get the rendering context for WebGL
-  gl = getWebGLContext(canvas);
+  gl = canvas.getContext("webgl", {preserveDrawingBuffer: true});
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
     return;
@@ -54,17 +56,57 @@ function connectVariablesToGLSL(){
     return;
   }
 
+  u_Size = gl.getUniformLocation(gl.program, 'u_Size');
+  if (!u_Size) {
+    console.log('Failed to get the storage location of u_Size');
+    return;
+  }
+
+}
+const POINT = 0; 
+const TRIANGLE = 1; 
+const CIRCLE = 2; 
+
+let g_selectedColor= [1.0,1.0,1.0,1.0];
+let g_selectedSize = 5;
+let g_selectedType=POINT; 
+let g_selectedSeg = 10;
+
+function addActionsForHtmIUI(){
+  //Button Events 
+  document.getElementById('green').onclick = function() { g_selectedColor = [0.0, 1.0, 0.0, 1.0]; };
+  document.getElementById('red').onclick = function() { g_selectedColor = [1.0, 0.0, 0.0, 1.0]; };
+  document.getElementById('clearButton').onclick = function() {g_shapesList=[]; renderAllshapes();}; 
+
+  document.getElementById('pointButton').onclick = function() {g_selectedType=POINT};
+  document.getElementById('triButton').onclick = function() {g_selectedType=TRIANGLE};
+  document.getElementById('circleButton').onclick = function() {g_selectedType=CIRCLE};
+
+  document.getElementById('segSlide').addEventListener('mouseup', function() { g_selectedSeg = this.value; });
+
+
+  //Color Slider Events
+  document.getElementById('redSlide').addEventListener('mouseup', function() { g_selectedColor[0] = this.value/100; });
+  document.getElementById('greenSlide').addEventListener('mouseup', function() { g_selectedColor[1] = this.value/100; });
+  document.getElementById('blueSlide').addEventListener('mouseup', function() { g_selectedColor[2] = this.value/100; });  
+
+  //Size Slider Events
+  document.getElementById('sizeSlice').addEventListener('mouseup', function() { g_selectedSize = this.value;});
 }
 
 
 function main() {
 
+  // setup canvas and gl variables
   setupWebGL(); 
+  // setup glsl programs and connect glsl variables 
   connectVariablesToGLSL();
+  addActionsForHtmIUI();
 
 
   // Register function (event handler) to be called on a mouse press
-  canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_Position, u_FragColor) };
+  canvas.onmousedown = click; 
+  canvas.onmousemove = function(ev) { if(ev.buttons == 1) { click(ev) } };
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -73,9 +115,36 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-var g_points = [];  // The array for the position of a mouse press
-var g_colors = [];  // The array to store the color of a point
-function click(ev, gl, canvas, a_Position, u_FragColor) {
+var g_shapesList = []; 
+
+
+function click(ev) {
+  let [x,y] = convertCoordinatesEventToGL(ev); 
+
+  let point; 
+  if (g_selectedType == POINT){
+    point = new Point(); 
+  }
+  else if(g_selectedType == TRIANGLE){
+    point = new Triangle();
+  }
+  else {
+    point = new Circle(); 
+    point.segments = g_selectedSeg;
+  }
+
+  point.position = [x,y]; 
+  point.color = g_selectedColor.slice(); 
+  point.size = g_selectedSize;
+  g_shapesList.push(point); 
+
+  
+  renderAllshapes();
+  
+
+}
+
+function convertCoordinatesEventToGL(ev){
   var x = ev.clientX; // x coordinate of a mouse pointer
   var y = ev.clientY; // y coordinate of a mouse pointer
   var rect = ev.target.getBoundingClientRect();
@@ -83,30 +152,33 @@ function click(ev, gl, canvas, a_Position, u_FragColor) {
   x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
   y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
 
-  // Store the coordinates to g_points array
-  g_points.push([x, y]);
-  // Store the coordinates to g_points array
-  if (x >= 0.0 && y >= 0.0) {      // First quadrant
-    g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
-  } else if (x < 0.0 && y < 0.0) { // Third quadrant
-    g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
-  } else {                         // Others
-    g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
-  }
+  return([x,y]);
+}
+
+function renderAllshapes(){
+
+  var startTime = performance.now();
 
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  var len = g_points.length;
+  var len = g_shapesList.length;
   for(var i = 0; i < len; i++) {
-    var xy = g_points[i];
-    var rgba = g_colors[i];
-
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
+    g_shapesList[i].render();
   }
+
+  // Check the time at the end of the function, and show on web page
+  var duration = performance.now() - startTime;
+  sendTextToHTML("numdot: " + len + " ms: " + Math.floor(duration) + " fps: " + Math.floor(10000/duration), "numdot");
+
+}
+
+// Set the text of a HTML element
+function sendTextToHTML(text, htmlID) {
+  var htmlElm = document.getElementById(htmlID);
+  if (!htmlElm) {
+    console.log("Failed to get " + htmlID + " from HTML");
+    return;
+  }
+  htmlElm.innerHTML = text;
 }
