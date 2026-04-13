17a1114074f6526392de81a25e574f85279018ad
justinisaiah73 <justinisaiah73@gmail.com>
2026-04-12 23:42:34 -0700

    finished asgn1

--- asgn1/asg1.js
+++ asgn1/asg1.js
@@ -159,13 +159,9 @@ function click(ev) {
 }
 
 function convertCoordinatesEventToGL(ev){
-  var x = ev.clientX; // x coordinate of a mouse pointer
-  var y = ev.clientY; // y coordinate of a mouse pointer
-  var rect = ev.target.getBoundingClientRect();
-
-  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
-  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
-
+  var rect = canvas.getBoundingClientRect();
+  var x = ((ev.clientX - rect.left) - rect.width/2)/(rect.width/2);
+  var y = (rect.height/2 - (ev.clientY - rect.top))/(rect.height/2);
   return([x,y]);
 }
 
